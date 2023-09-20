a74eb4bc7e45a97bac4f0934d5ea148a755de11b
Andrewtjx1 <76143067+Andrewtjx1@users.noreply.github.com>
2023-09-20 15:48:19 +0800

    Added CSVWriter class to write results to csv

---
+++ Scenarios/CSVWriter.py
@@ -0,0 +1,38 @@
+"""
+Author : Tan Jian Xi
+last modified: 20/9
+last modified by: Tan Jian Xi
+"""
+
+import csv
+
+"""
+Information to be recorded:
+-DIST TRAVELLED RANDOM
+-TIME TAKEN RANDOM
+-DIST TRAVELLED HEURISTIC
+-TIME TAKEN HEURISTIC
+-DIST TRAVELLED OPTIMAL
+-TIME TAKEN OPTIMAL
+-NO OF CROSS AISLES
+-NO OF PICKERS
+-GENERATION SEED(FOR SKU PLACEMENT)
+"""
+CSVHEADER = ["SEED","DIST TRAVELLED RANDOM","TIME TAKEN RANDOM","DIST TRAVELLED HEURISTIC","TIME TAKEN HEURISTIC","DIST TRAVELLED OPTIMAL","TIME TAKEN OPTIMAL","NO OF CROSS AISLES","NO OF PICKERS"]
+
+def writeNewCsv(filename:str,data:list[list]) -> bool:
+    fields = CSVHEADER
+
+    with open(filename,"w") as csvfile:
+        # creating a csv writer object 
+        csvwriter = csv.writer(csvfile) 
+        
+        # writing the fields 
+        csvwriter.writerow(fields) 
+        
+        # writing the data rows 
+        csvwriter.writerows(data)
+    
+    return True
+
+    
